3bfc869627ac422b3920e06a3527fd14d352f168
Gabor Juhasz <juhg24@gmail.com>
2016-09-07 14:11:49 +0200

    disabled IP checking

--- web/index.php
+++ web/index.php
@@ -1,26 +1,5 @@
 <?php
 
-if ( get_client_ip() !== '85.66.6.243' && get_client_ip() !== '127.0.0.1' ) exit;
-
-function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
-
 /** WordPress view bootstrapper */
 define('WP_USE_THEMES', true);
 require(__DIR__ . '/wp/wp-blog-header.php');
